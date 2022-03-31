3b85edbedca5e8f13411b68037359bf1513744c3
intellij_zhich <zhicheng@gmail.com>
2022-03-31 17:28:40 +0900

    [2022/3/31][3度目の変更]

--- src/main/java/sample/HelloWorldSample.java
+++ src/main/java/sample/HelloWorldSample.java
@@ -12,6 +12,26 @@ public class HelloWorldSample {
 
         SamplePojo pojo = new SamplePojo();
         pojo.setField(100);
-        System.out.println(pojo.getField());
+        // System.out.println(pojo.getField());
+
+        sampleCast();
+        sampleInstanceOf();
+    }
+
+    private static void sampleCast() {
+        Cat c = new Cat();
+        Animal a = c; // アップキャスト
+        Cat c1 = (Cat) a; // ダウンキャスト
+    }
+
+    private static void sampleInstanceOf() {
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+        Animal animal = cat;
+
+        System.out.println(cat instanceof Cat); // true
+        System.out.println(cat instanceof Animal); // true
+        System.out.println(animal instanceof Cat); // true 実行時に Cat 型となるため
+        System.out.println(animal instanceof Dog); // false
     }
 }
